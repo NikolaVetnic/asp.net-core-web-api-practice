f556708e1b000bdbe040719a4057651f19d6b5b7
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-09-10 12:46:00 +0200

    feat: add product domain entity

---
+++ EShopMicroservices/src/Services/Catalogue/Catalogue.Api/Modules/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalogue.Api.Modules;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public List<string> Category { get; set; } = [];
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
+}
