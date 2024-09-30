89b07f8616bacc0f98bb118120933df99eb3f984
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-09-30 11:14:51 +0200

    feat: implement product creation

---
+++ EShopMicroservices/src/Services/Catalogue/Catalogue.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.Cqrs;
+using Catalogue.Api.Models;
+
+namespace Catalogue.Api.Products.CreateProduct;
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // todo: Save to database
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
