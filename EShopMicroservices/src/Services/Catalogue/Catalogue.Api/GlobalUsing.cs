3bcefc1d907b056f9add43005a69fee5470fec3c
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-10-08 17:20:25 +0200

    feat: add health checks

--- EShopMicroservices/src/Services/Catalogue/Catalogue.Api/GlobalUsing.cs
+++ EShopMicroservices/src/Services/Catalogue/Catalogue.Api/GlobalUsing.cs
@@ -2,11 +2,13 @@
 global using FluentValidation;
 global using Mapster;
 global using Marten;
+global using Marten.Pagination;
 global using MediatR;
 
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Cqrs;
 global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.Exceptions.Handler;
+global using Catalogue.Api.Data;
 global using Catalogue.Api.Exceptions;
 global using Catalogue.Api.Models;
