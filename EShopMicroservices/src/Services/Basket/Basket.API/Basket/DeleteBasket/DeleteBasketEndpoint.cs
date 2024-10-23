0ee21b5518066f4ddddc874e9cce43b965035260
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-10-23 22:56:27 +0200

    basket/chore: fix typo

--- EShopMicroservices/src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ EShopMicroservices/src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -4,9 +4,9 @@ public class DeleteBasketEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+        app.MapDelete("/basket/{username}", async (string username, ISender sender) =>
             {
-                var result = await sender.Send(new DeleteBasketCommand(userName));
+                var result = await sender.Send(new DeleteBasketCommand(username));
                 var response = result.Adapt<DeleteBasketResponse>();
 
                 return Results.Ok(response);
@@ -20,6 +20,6 @@ public class DeleteBasketEndpoint : ICarterModule
     }
 }
 
-// public record DeleteBasketRequest(string UserName);
+// public record DeleteBasketRequest(string Username);
 
 public record DeleteBasketResponse(bool IsSuccess);
