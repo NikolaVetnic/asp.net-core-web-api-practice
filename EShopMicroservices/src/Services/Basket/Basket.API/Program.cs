77a8ed4f85697de5e458e4e311269dd7cfad1f08
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-10-23 22:56:50 +0200

    basket/feat: register carter and mediatr

--- EShopMicroservices/src/Services/Basket/Basket.API/Program.cs
+++ EShopMicroservices/src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
