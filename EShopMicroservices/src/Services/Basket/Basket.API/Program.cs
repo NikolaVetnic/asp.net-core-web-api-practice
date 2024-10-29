c65bcf9202bd7394a1b4c07c4a460a628170897a
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-10-29 13:35:13 +0100

    basket/feat: register exception handler

--- EShopMicroservices/src/Services/Basket/Basket.API/Program.cs
+++ EShopMicroservices/src/Services/Basket/Basket.API/Program.cs
@@ -19,10 +19,13 @@ builder.Services.AddMarten(options =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
+
+app.UseExceptionHandler(_ => { });
 
 app.Run();
