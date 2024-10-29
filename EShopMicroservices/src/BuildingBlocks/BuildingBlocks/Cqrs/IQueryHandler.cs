c110968093dfa7f6164e1512a05dfed4aa08e450
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-10-29 13:29:39 +0100

    building-blocks/chore: remove empty lines

--- EShopMicroservices/src/BuildingBlocks/BuildingBlocks/Cqrs/IQueryHandler.cs
+++ EShopMicroservices/src/BuildingBlocks/BuildingBlocks/Cqrs/IQueryHandler.cs
@@ -2,8 +2,6 @@
 
 namespace BuildingBlocks.Cqrs;
 
-
-
 public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse> 
     where TQuery : IQuery<TResponse> 
     where TResponse : notnull { }
