709c7d92cfaf09f005ad43680bc433dafe857359
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-11-18 22:48:41 +0100

    basket/feat: add sqlite db and redis cache health checks

--- EShopMicroservices/src/Services/Basket/Basket.API/Program.cs
+++ EShopMicroservices/src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,6 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
@@ -28,11 +31,18 @@ builder.Services.AddStackExchangeRedisCache(options =>
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
-
 app.UseExceptionHandler(_ => { });
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
