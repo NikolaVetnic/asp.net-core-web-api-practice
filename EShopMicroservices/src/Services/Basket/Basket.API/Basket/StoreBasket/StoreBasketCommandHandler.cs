0659983c457eb3d422ef6defc8fd9d14cf33f5c8
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-11-27 05:37:13 +0100

    basket/feat: consumer discount.grpc service and deduct discount on storing basket

--- EShopMicroservices/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
+++ EShopMicroservices/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,17 +1,33 @@
-﻿namespace Basket.Api.Basket.StoreBasket;
+﻿using Discount.Grpc;
 
-public class StoreBasketCommandHandler(IBasketRepository repository) 
+namespace Basket.Api.Basket.StoreBasket;
+
+public class StoreBasketCommandHandler(
+    IBasketRepository repository,
+    DiscountProtoService.DiscountProtoServiceClient discountProto)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        // todo: communicate with grpc Discount service
-        
-        
+        // communicate with grpc Discount service
+        await DeductDiscounts(command.Cart, cancellationToken);
+
         // store basket in database (use Marten upsert - if exists update, if not create) and update cache
         await repository.StoreBasket(command.Cart, cancellationToken);
 
         return new StoreBasketResult(command.Cart.Username);
+    }
+
+    private async Task DeductDiscounts(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        // communicate with Discount.Grpc and calculate discounted prices of products into shopping cart
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(
+                new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+
+            item.Price -= coupon.Amount;
+        }
     }
 }
 
--- EShopMicroservices/src/Services/Basket/Basket.API/Program.cs
+++ EShopMicroservices/src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,4 @@
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
@@ -33,6 +34,22 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.InstanceName = "Basket";
 });
 
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+    {
+        options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+    })
+    .ConfigurePrimaryHttpMessageHandler(() =>
+    {
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+
+        return handler;
+    });
+
+// Add services to the container - Cross-Cutting Concerns
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddHealthChecks()
