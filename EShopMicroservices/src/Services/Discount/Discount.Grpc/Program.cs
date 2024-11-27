6007c9b7ee0d37d27deab08831733e4313d33027
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-11-27 05:47:45 +0100

    discount/feat: configure kestrel to work on macos

--- EShopMicroservices/src/Services/Discount/Discount.Grpc/Program.cs
+++ EShopMicroservices/src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,6 @@
 using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,24 @@ builder.Services.AddGrpc();
 
 builder.Services.AddDbContext<DiscountContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("Database")));
+
+// This was required to circumvent the "The SSL connection could not be established. Cannot determine the frame size or a corrupted frame was received." exception
+builder.WebHost.ConfigureKestrel(options =>
+{
+    var env = builder.Environment.EnvironmentName;
+
+    options.ListenAnyIP(8080, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2; // Listen on port 8080 for Docker
+    });
+
+    // Optional: Add development fallback
+    if (env == "Development")
+        options.ListenAnyIP(5002, listenOptions =>
+        {
+            listenOptions.Protocols = HttpProtocols.Http2; // Local testing fallback
+        });
+});
 
 var app = builder.Build();
 
