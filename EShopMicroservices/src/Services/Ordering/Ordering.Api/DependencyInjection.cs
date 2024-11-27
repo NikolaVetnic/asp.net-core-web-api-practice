29a2ce3f11172a5c301767b73224a6ccf9fe6b98
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-11-27 05:50:29 +0100

    ordering/feat: add api, application, domain and infrastructure projects

---
+++ EShopMicroservices/src/Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -0,0 +1,18 @@
+namespace Ordering.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        // services.AddCarter();
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        // app.MapCarter();
+
+        return app;
+    }
+}
